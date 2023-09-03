418b78a2bda4efa0ff7b30ae29e6f7778803f55d
hemant14050 <hemant14050@gmail.com>
2023-09-03 12:03:31 +0530

    modified some unwanted css changes on events

--- server/public/js/leftPane.js
+++ server/public/js/leftPane.js
@@ -3,15 +3,19 @@ const showMenuBtnNode = document.querySelector('#showMenuBtn');
 const closeBtnNode = document.querySelector('#closeBtn');
 
 closeBtnNode.addEventListener('click', () => {
-    leftMenuNode.style = 'display: none';
+    if(!leftMenuNode.classList.contains("hidden")) {
+        leftMenuNode.classList.add("hidden");
+    }
 });
 
 showMenuBtnNode.addEventListener('click', () => {
-    leftMenuNode.style = 'display: flex';
+    leftMenuNode.classList.remove("hidden");
 });
 
 document.addEventListener('click', (e) => {
     if (e.target.id !== 'showMenuBtn' && e.target.id !== 'closeBtn' && e.target.id !== 'leftMenu') {
-        leftMenuNode.style = 'display: none';
+        if(!leftMenuNode.classList.contains("hidden")) {
+            leftMenuNode.classList.add("hidden");
+        }
     }
 });
